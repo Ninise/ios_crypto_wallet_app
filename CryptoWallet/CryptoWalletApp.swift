0515508165c017439e998f24ddb23532219a3d69
Nikita Nikitin <nikitin.ninise@gmail.com>
2022-12-13 12:04:34 +0200

    Initial Commit

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Nikita on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
